b9529c98ab6552713f3c5f1ef0ea7f27349ce1a1
agospaladino <apaladino@udesa.edu.ar>
2025-09-16 19:28:29 -0300

    Arranco Home.js

--- frontend/src/screens/Home/Home.js
+++ frontend/src/screens/Home/Home.js
@@ -1,14 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Pelicula from '../../Components/Pelicula/Pelicula';
 
-function Home(){
-    return(
-        <React.Fragment>
-            <h1>Este es el home</h1>
-            <p>aca vamos a ver las peliculas populares y las que estan en cartelera</p>
-            <p>eso lo vamos a hacer haciendo dos componentes</p>
-        </React.Fragment>
+let apiKey = "0b687acd5250ddef9e6794dc722be275";
 
-    )
+class Home extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            peliculasPopulares: [],
+            peliculasCartelera: [],
+            cargandoPopulares: true,
+            cargandoCartelera: true
+        }
+    }
+    componentDidMount(){
+        fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}`)
+        .then(respuesta => respuesta.json())
+        .then(data => this.setState({
+            peliculasPopulares: data.results,
+            cargandoPopulares: false
+        }))
+        .catch(error => console.log(error))
+
+        fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}`)
+        .then(respuesta => respuesta.json())
+        .then(data => this.setState({
+            peliculasCartelera: data.results,
+            cargandoCartelera: false
+        }))
+        .catch(error => console.log(error))
+    }
+    render(){
+        return(
+            <div className="container">
+                <h2 class="alert alert-primary">Popular movies this week</h2>
+                <Pelicula peliculas={this.state.peliculasPopulares} />
+            </div>
+        )
+    }
 }
 
 export default Home;
